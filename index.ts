b468015246438c1a65e6f88419bd7be802270082
Albin Norback <albin.nojback@gmail.com>
2022-10-09 16:30:42 +0200

    Add JWT As Authorization

--- index.ts
+++ index.ts
@@ -1,15 +1,17 @@
 import dotenv from "dotenv";
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 import cookieParser from "cookie-parser";
 import { QueryTypes, Sequelize } from "sequelize";
 import cors from "cors";
 import * as tedious from "tedious";
 import crypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
+const secret = process.env.SECRET || "secret";
 
 app.use(express.json());
 app.use(cookieParser());
@@ -69,7 +71,9 @@ app.post("/register", async (req, res) => {
 		}
 	}
 
-	res.status(200).json(adminAccess);
+	const accessToken = jwt.sign({ email }, secret, { expiresIn: "1d" });
+
+	res.status(200).json({ adminAccess, accessToken });
 });
 
 app.post("/login", async (req, res) => {
@@ -99,148 +103,104 @@ app.post("/login", async (req, res) => {
 		}
 	}
 
-	res.status(200).json(adminAccess);
+	const accessToken = jwt.sign({ email }, secret, { expiresIn: "1d" });
+
+	res.status(200).json({ adminAccess, accessToken });
 	return;
 });
-/*
-		
-app.get("/inventory", (req, res) => {
-	const idToken = req.query.idToken;
 
-	if (typeof idToken !== "string") {
-		res.status(400).send("Bad Request, No ID Token");
-		return;
-	}
+app.get("/inventory", authenticateToken, async (req, res) => {
+	const email = (req as any).email;
 
-	console.time("Verify ID");
-
-	admin
-		.auth()
-		.verifyIdToken(idToken)
-		.then(async (decodedToken) => {
-			console.timeEnd("Verify ID");
-			console.time("Get Inventory");
-			let inventory = await sequelize.query(
-				`SELECT Cases.CaseName, Cases.ImagePath, SUM(InventoryDetails.Quantity) AS Quantity
+	let inventory = await sequelize.query(
+		`SELECT Cases.CaseName, Cases.ImagePath, SUM(InventoryDetails.Quantity) AS Quantity
 				FROM Users
 				INNER JOIN InventoryDetails
 				ON Users.ID = InventoryDetails.UserID
 				INNER JOIN Cases
 				ON InventoryDetails.CaseID = Cases.ID
-				WHERE Users.FirebaseUID = '${decodedToken.uid}' AND Quantity > 0
+				WHERE Users.Email = '${email}' AND Quantity > 0
 				GROUP BY Cases.CaseName, Cases.ImagePath`,
-				{ type: QueryTypes.SELECT }
-			);
+		{ type: QueryTypes.SELECT }
+	);
 
-			console.timeEnd("Get Inventory");
-
-			res.status(200).json(inventory);
-		})
-		.catch((err) => {
-			console.log(err);
-			res.status(401).send("Unauthorized Request");
-		});
+	res.status(200).json(inventory);
 });
 
-app.delete("/case", (req, res) => {
-	const idToken = req.body.idToken || "";
+app.delete("/case", authenticateToken, async (req, res) => {
+	const email = (req as any).email;
 	const caseName = req.body.caseName;
 
-	if (typeof idToken !== "string" || typeof caseName !== "string") {
-		console.log("Bad Request, No ID Token or Case Name");
-		res.status(400).json(false);
-		return;
-	}
-
-	admin
-		.auth()
-		.verifyIdToken(idToken)
-		.then(async (decodedToken) => {
-			let intventoryDetails: any[] = await sequelize.query(
-				`SELECT InventoryDetails.ID, InventoryDetails.Quantity FROM Users
+	let intventoryDetails: any[] = await sequelize.query(
+		`SELECT InventoryDetails.ID, InventoryDetails.Quantity FROM Users
 				INNER JOIN InventoryDetails
 				ON Users.ID = InventoryDetails.UserID
 				INNER JOIN Cases
 				ON InventoryDetails.CaseID = Cases.ID
-				WHERE Cases.CaseName = '${caseName}' AND Users.FirebaseUID = '${decodedToken.uid}'`,
-				{ type: QueryTypes.SELECT }
-			);
+				WHERE Cases.CaseName = '${caseName}' AND Users.Email = '${email}'`,
+		{ type: QueryTypes.SELECT }
+	);
 
-			if (intventoryDetails.length === 0) {
-				console.log("No Records Found");
-				res.status(200).json(false);
-				return;
-			}
-
-			let removed = false;
-
-			for (let i = 0; i < intventoryDetails.length; i++) {
-				if (intventoryDetails[i].Quantity === 1 && !removed) {
-					await sequelize.query(
-						`DELETE FROM InventoryDetails WHERE InventoryDetails.ID = ${intventoryDetails[i].ID}`
-					);
-					removed = true;
-				} else if (intventoryDetails[i].Quantity > 1 && !removed) {
-					await sequelize.query(
-						`UPDATE InventoryDetails SET InventoryDetails.Quantity = ${
-							intventoryDetails[i].Quantity - 1
-						} WHERE InventoryDetails.ID = ${
-							intventoryDetails[i].ID
-						}`
-					);
-					removed = true;
-				} else if (intventoryDetails[i].Quantity === 0) {
-					await sequelize.query(
-						`DELETE FROM InventoryDetails WHERE InventoryDetails.ID = ${intventoryDetails[i].ID}`
-					);
-				}
-			}
-
-			console.log(removed);
-
-			res.status(200).json(removed);
-			return;
-		})
-		.catch((err) => {
-			console.log(err);
-			res.status(401).json(false);
-			return;
-		});
-});
-
-app.get("/items", (req, res) => {
-	const idToken = req.query.idToken || "";
-	const caseName = req.query.caseName;
-
-	if (typeof idToken !== "string" || typeof caseName !== "string") {
-		res.status(400).send("Bad Request, No ID Token or Case Name");
+	if (intventoryDetails.length === 0) {
+		console.log("No Records Found");
+		res.status(200).json(false);
 		return;
 	}
 
-	admin
-		.auth()
-		.verifyIdToken(idToken)
-		.then(async (decodedToken) => {
-			let items = await sequelize.query(
-				`SELECT Items.ItemName, Items.ImagePath, Items.Rarity FROM Items
+	let removed = false;
+
+	for (let i = 0; i < intventoryDetails.length; i++) {
+		if (intventoryDetails[i].Quantity === 1 && !removed) {
+			await sequelize.query(
+				`DELETE FROM InventoryDetails WHERE InventoryDetails.ID = ${intventoryDetails[i].ID}`
+			);
+			removed = true;
+		} else if (intventoryDetails[i].Quantity > 1 && !removed) {
+			await sequelize.query(
+				`UPDATE InventoryDetails SET InventoryDetails.Quantity = ${
+					intventoryDetails[i].Quantity - 1
+				} WHERE InventoryDetails.ID = ${intventoryDetails[i].ID}`
+			);
+			removed = true;
+		} else if (intventoryDetails[i].Quantity === 0) {
+			await sequelize.query(
+				`DELETE FROM InventoryDetails WHERE InventoryDetails.ID = ${intventoryDetails[i].ID}`
+			);
+		}
+	}
+
+	res.status(200).json(removed);
+	return;
+});
+
+app.get("/items", async (req, res) => {
+	const caseName = req.query.caseName;
+
+	let items = await sequelize.query(
+		`SELECT Items.ItemName, Items.ImagePath, Items.Rarity FROM Items
 				INNER JOIN Cases
 				ON Items.CaseID = Cases.ID
 				WHERE Cases.CaseName = '${caseName}'`,
-				{ type: QueryTypes.SELECT }
-			);
+		{ type: QueryTypes.SELECT }
+	);
 
-			res.status(200).json(items);
-		})
-		.catch((err) => {
-			console.log(err);
-			res.status(401).send("Unauthorized");
-		});
+	res.status(200).json(items);
 });
-*/
 
 app.listen(port, () => {
 	console.log(`Listening on http://localhost:${port}`);
 });
+
+function authenticateToken(req: Request, res: Response, next: NextFunction) {
+	const token = req.headers["authorization"];
+	if (!token) return res.sendStatus(401);
+
+	jwt.verify(token, secret, (err, user: any) => {
+		if (err) return res.sendStatus(403);
+		(req as any).email = user.email;
+		next();
+	});
+}
 
 async function addCasesToUser(
 	userID: number,
